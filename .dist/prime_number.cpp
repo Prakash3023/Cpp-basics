ae4a69b6c1cc3161209a06f1c7ebdba04d3168e5
Prakash3023 <prakashjyotidas698@gmail.com>
2024-10-14 10:39:31 +0530

    new commit

---
+++ .dist/prime_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count=0;
+    int num=5;
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+       count++;
+        }
+    }
+    if(count==2){
+        cout<<"It is a Prime number";
+    }
+    else{
+        cout<<"It is  Not a Prime number";
+    }
+}
