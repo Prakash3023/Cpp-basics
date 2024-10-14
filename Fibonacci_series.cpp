8ae9b626a491a4167d17a806c127971d1d4c8817
Prakash3023 <prakashjyotidas698@gmail.com>
2024-10-14 17:43:23 +0530

    new commit

---
+++ Fibonacci_series.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+     int T1 = 0, T2 = 1, nexterm;
+    cout << "Fibonacci Series: " << T1 << ", " << T2;
+    for (int i = 3; i <= n; ++i) {
+        nexterm = T1 + T2;
+        cout << ", " << nexterm;
+        T1 = T2;
+        T2 = nexterm;
+    }
+    cout << endl;
+    return 0;
+}
