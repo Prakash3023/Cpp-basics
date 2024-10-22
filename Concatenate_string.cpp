9d183ae38fd8871446ed4a877a9b54232be26727
Prakash3023 <prakashjyotidas698@gmail.com>
2024-10-22 16:34:52 +0530

    new commit

---
+++ Concatenate_string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str1("Hello");
+    string str2(" World");
+    str1.append(str2);
+
+    cout << str1 << endl;
+    return 0;
+}
