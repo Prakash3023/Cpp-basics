23858254a150dcac389a6a503d5e6e4be45bd873
Prakash3023 <prakashjyotidas698@gmail.com>
2024-11-12 00:33:13 +0530

    new commit

---
+++ Reverse_strinng.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void reverse(char name[]) {
+    int s = 0;
+    int e = strlen(name) - 1;
+    
+    while (s < e) {
+        swap(name[s++], name[e--]);
+    }  
+} 
+
+int main() {
+    char name[20];
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your name is: " << name << endl;
+     
+    reverse(name);
+    cout << "Reversed String: " << name << endl;
+     
+    return 0;
+}
+ 
