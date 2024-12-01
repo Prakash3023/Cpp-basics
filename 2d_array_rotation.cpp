dc34394beda4d7bd7ebd7078341c6ef442dd824f
Prakash3023 <prakashjyotidas698@gmail.com>
2024-12-01 23:23:47 +0530

    new commit

---
+++ 2d_array_rotation.cpp
@@ -0,0 +1,49 @@
+ #include <iostream>
+#include <vector>
+#include <algorithm> // Include for reverse function
+using namespace std;
+
+void rotateMatrix(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+void displayMatrix(const vector<vector<int>> &matrix) {
+    for (const auto &row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the matrix (NxN): ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Original Matrix:" << endl;
+    displayMatrix(matrix);
+
+    rotateMatrix(matrix);
+
+    cout << "Rotated Matrix:" << endl;
+    displayMatrix(matrix);
+
+    return 0;
+}
